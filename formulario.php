48661d4940780106606192e65532d4773a6e415a
Alastor003 <bri.ferreira03@gmail.com>
2022-11-13 20:35:35 -0300

    proyecto actualizado 13/11

--- formulario.php
+++ formulario.php
@@ -1,78 +1,74 @@
-<?php require_once('conf/conf.php')    ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital@1&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/contacto.css">
-    <link rel="stylesheet" href="css/estilo.css">
-    <link rel="stylesheet" href="css/normalize.css">
-    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <title>PortalGaming</title>
-</head>
-<body>
+<?php require_once('conf/conf.php') ?>
+<?php require('layout/_header.php'); ?>
+<?php require_once('funciones/funciones_input.php'); ?>
+<?php
+$nombre = test_input($_POST['nombre'] ?? null);
+$mensaje = test_input($_POST['mensaje'] ?? null);
+$email = test_input($_POST['email'] ?? null);
+$asunto = test_input($_POST['asunto'] ?? null);
 
-    <nav>
-        <input type="checkbox" id="check">
-        <label for="check" class="botoncheck">
-            <i class="fas fa-bars"></i>
-        </label>
-        <img id="logoimg" src="img/logo.jpg" alt="logoPortalGaming">
-        <label class="logo" >PORTAL<span class="house">GAMING</span> </label>
-            <ul class="header_ul">
-                <li><a class="btn_nav" href="index.php">HOME</a></li>
-                <li><a class="btn_nav" href="products.php"> PRODUCTOS</a></li>
-                <li><a class="btn_nav" href="noticias.php"> NOTICIAS</a></li>
-                <li><a class="btn_nav" href="formulario.php"> CONTACTO</a></li>
-               
-                
-            </ul>
-    </nav>
+$errores = array();
 
-<main class="contenedor-sombra">
-      
+if ( isset($_POST['submit']) ){
 
-      <section>
-         <h1 class="formt">Formulario de contacto</h1>
-         <form class="formulario">
-            <fieldset>
-                  <legend>Contactanos</legend>
-                  <div class="contenedor-campos">
-                  <div class="campo">
-                     <label for="nombre">Nombre</label>
-                     <input class="input-text" type="text" placeholder="Tu nombre" id="nombre">
-                  </div>  
-                  <div class="campo">
-                     <label for="telefono">Telefono</label>
-                     <input class="input-text" type="tel" placeholder= "Tu telefono" id="telefono" required>
-                  </div> 
-                  <div class="campo">
-                     <label for="correo"> Correo </label>
-                     <input class="input-text" type= "text" placeholder= "Tu Email" id="correo" required>
-                  </div>
-                  <div class="campo">
-                     <label for="mensaje">Mensaje</label>
-                     <textarea class="input-text" placeholder="Escriba aqui su consulta" id="mensaje"></textarea>
-                  </div>
-                  </div>
-      
-                  <!--boton-->
-                  <div class="alinear-derecha flex">
-                  <input id="enviar" class="boton w-sm-100" type="submit" value="Enviar">
-               </div>
-         <form/>            
-      </section>
-</main>
-      <script src="js/script.js"></script>
-      <?php require('layout/_footer.php'); ?>
+    if( empty($nombre) ){
+        array_push($errores, 'Debes ingresar un nombre');
+    }
+
+    if( filter_var($email, FILTER_VALIDATE_EMAIL ) == FALSE ) {
+        array_push($errores, 'Ingrese un mail valido');
+    }
+    
+    if( empty($asunto) ) {
+        array_push($errores, 'Ingrese un asunto al mensaje');
+    }
+    if( empty($mensaje) ) {
+        array_push($errores, 'Ingrese un Mensaje');
+    }
+}
+?>
+
+<section class="contenedor_form">
+    <h1 class= "tituform">Contactanos</h1>
+
+        <div class="contacto-form">
+          <form class="form-grid" action="formulario.php" method="post">
+            <div>
+                <label class="text-form" for="nombre"></label>
+                <input placeholder="Nombre" class="area" type ="text" name ="nombre" id="nombre">
+            </div>
+            <div>
+                <label class="text-form" for="email"></label> 
+                <input placeholder="email" class="area" type ="email"  name="email" id="email">
+            </div>
+            <div>
+                <label class="text-form" for="telefono"></label>
+                <input placeholder="telefono" class="area" type ="number" Tel ="Telefono" id="telefono">
+            </div>
+            <div>
+                <label class="text-form" for="asunto"></label>
+                <input placeholder="asunto" class="area" type ="text" name="asunto" id="asunto">
+            </div>
+            <div class="grid-mensaje">        
+            <label for="mensaje" class="text-form" ></label>
+            <textarea placeholder="Escribe tu mensaje"  class="area" id="mensaje" name="mensaje" rows="3"></textarea>
+            </div>        
+            <div class="grid-boton">
+            <button class="boton-form" type="submit" name="submit" >
+            Enviar
+            </button>
+            </div>
+         </form>   
+        </div>    
+        <ul>
+            <?php foreach($errores as $error){ ?>
+                <li class="error"> <?php echo $error ?> </li>
+            <?php } ?>
+        </ul>
+</section>
+
+
+<?php require('layout/_footer.php'); ?>
 
 
 
